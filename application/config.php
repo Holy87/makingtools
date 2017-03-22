61686cd6da1d6f915598568f809359d90759e9ca
Holy87 <fr.bosso@outlook.it>
2017-03-22 21:55:36 +0100

    Aggiunta del file config

---
+++ application/config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Francesco
+ * Date: 22/03/2017
+ * Time: 21:49
+ */
+
+// -- DATABASE SETTINGS
+
+// Database host
+define('DB_HOST', 'localhost');
+// Database name
+define('DB_NAME', 'rm_tools');
+// User login
+define('DB_USER', 'root');
+// User password
+define('DB_PASS', '');
+
+if ( !defined('ABS_PATH') )
+    define('ABS_PATH', dirname(__FILE__) . '/');
