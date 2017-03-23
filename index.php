fcb4f4f6dc18556f079676fefceed67e5c8e2a8a
Holy87 <fr.bosso@outlook.it>
2017-03-23 23:50:12 +0100

    Aggiunta DB Connection

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
  * Date: 21/03/2017
  * Time: 21:11
  */
+include_once 'config.php';
 include_once 'application/controllers/controller.php';
+include_once 'application/db_connection.php';
+
 $controller = new Controller();
 $controller->invoke();
