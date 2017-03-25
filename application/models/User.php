23fa57c8f6b008e4b341095592996ab3c2d2fba7
Francesco Bosso <Francesco Bosso>
2017-03-25 19:57:14 +0100

    Aggiornamento

--- application/models/User.php
+++ application/models/User.php
@@ -11,11 +11,25 @@ class User
     public $id;
     public $username;
     public $mail;
-
-    public function __construct($id, $username, $mail)
+    public $state;          //0: idle, 1: active, -1: banned
+    public $avatar_url;     //image name
+    
+    
+    /**
+     * Main constructor
+     * @param int $id
+     * @param string $username
+     * @param string $mail
+     * @param int $active
+     * @param int $banned
+     * @param string $avatar_url
+     **/
+    public function __construct($id, $username, $mail, $active, $avatar_url)
     {
         $this->id = $id;
         $this->username = $username;
         $this->mail = $mail;
+        $this->state = $active;
+        $this->avatar_url = $avatar_url;
     }
 }
